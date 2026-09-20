93298c468463e472399a1f50bed6974ea76ba79d
Jimb Esser <wasteland@gmail.com>
2026-09-20 16:12:32 -0700

    Wrapping up

--- push_web.cmd
+++ push_web.cmd
@@ -1,1 +1,3 @@
-xcopy /s /y dist\game\build.dev\client\*.* W:\thesilentb.com\gb14\test1\
+node build build
+xcopy /s /y dist\game\build.prod\client\*.* W:\thesilentb.com\gb14\test1\
+xcopy /s /y dist\game\build.prod\client\*.* C:\Code\SRC2\web\dashingstrike.com\LudumDare\GBJ14\
