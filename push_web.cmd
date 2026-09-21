ea78b8893b8510993cdcbaa48a97f2de96be0fe8
Jimb Esser <wasteland@gmail.com>
2026-09-20 17:51:21 -0700

    CD update

--- push_web.cmd
+++ push_web.cmd
@@ -1,3 +1,20 @@
 node build build
 xcopy /s /y dist\game\build.prod\client\*.* W:\thesilentb.com\gb14\test1\
-xcopy /s /y dist\game\build.prod\client\*.* C:\Code\SRC2\web\dashingstrike.com\LudumDare\GBJ14\
+
+@SET TORTOISE_PATH=%ProgramW6432%\TortoiseSVN\bin\TortoiseProc.exe
+@SET DEST=..\..\SRC2\web\dashingstrike.com\LudumDare\GBJ14\
+
+call node build build
+
+rd /s /q %DEST%
+md %DEST%
+xcopy /SY dist\game\build.prod\client %DEST%
+
+"%TORTOISE_PATH%" /command:commit /path:%DEST%  /logmsg:"GBJ14"
+
+@pushd ..\..\SRC2\flightplans
+
+@echo.
+@echo.
+@echo NEXT: Run `npm run web-prod` in the Node 12 shell
+@node12shell
