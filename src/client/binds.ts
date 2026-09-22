f9dc058c1f1a01b5ab2c8c8317dc0f260e1d345b
Jimb Esser <wasteland@gmail.com>
2026-09-22 12:47:55 -0700

    Allow binding a single key to multiple actions

--- src/client/binds.ts
+++ src/client/binds.ts
@@ -1,65 +1,73 @@
 import { cmd_parse } from 'glov/client/cmds';
 import { keyDownEdge, KEYS, keyUpEdge, PAD, padButtonDownEdge, padButtonUpEdge } from 'glov/client/input';
+import { Rec } from 'glov/common/types';
 
-type BindMode = 'hold' | 'fire';
-type KBBind = {
-  key: number;
+export type ValidKey = keyof typeof KEYS;
+export type ValidPad = keyof typeof PAD;
+export type BindMode = 'hold' | 'fire';
+
+type Bind = {
   cmd: string;
   mode: BindMode;
 };
-let kb_binds: KBBind[] = [];
-type PadBind = {
-  pad: number;
-  cmd: string;
-  mode: BindMode;
+type BindList = {
+  code: number;
+  list: Bind[];
 };
-let pad_binds: PadBind[] = [];
+let kb_binds: Rec<ValidKey, BindList> = {};
+let pad_binds: Rec<ValidPad, BindList> = {};
 
 export function bindKB(key: keyof typeof KEYS, cmd: string, mode: BindMode): void {
-  kb_binds.push({
-    key: KEYS[key],
+  kb_binds[key] = kb_binds[key] || { code: KEYS[key], list: [] };
+  kb_binds[key].list.push({
     cmd,
     mode,
   });
 }
 
 export function bindPad(pad: keyof typeof PAD, cmd: string, mode: BindMode): void {
-  pad_binds.push({
-    pad: PAD[pad],
+  pad_binds[pad] = pad_binds[pad] || { code: PAD[pad], list: [] };
+  pad_binds[pad].list.push({
     cmd,
     mode,
   });
 }
 
+const bind_set = [{
+  list: kb_binds,
+  downEdge: keyDownEdge,
+  upEdge: keyUpEdge,
+}, {
+  list: pad_binds,
+  downEdge: padButtonDownEdge,
+  upEdge: padButtonUpEdge,
+}];
+
 export function bindsCheck(): void {
   // TODO: allow overriding cmd_parse.handle with chatUI.cmdParse for binding to
   //   network actions and access level checks?
-  // TODO: deal with same key bound to multiple things?
-  for (let ii = 0; ii < kb_binds.length; ++ii) {
-    let bind = kb_binds[ii];
-    if (keyDownEdge(bind.key)) {
-      if (bind.mode === 'hold') {
-        cmd_parse.handle(undefined, `${bind.cmd} 1`);
-      } else {
-        cmd_parse.handle(undefined, bind.cmd);
+  for (let jj = 0; jj < bind_set.length; ++jj) {
+    let set = bind_set[jj];
+    for (let key in set.list) {
+      let bindlist = set.list[key as keyof typeof set.list]!;
+      if (set.downEdge(bindlist.code)) {
+        for (let ii = 0; ii < bindlist.list.length; ++ii) {
+          let bind = bindlist.list[ii];
+          if (bind.mode === 'hold') {
+            cmd_parse.handle(undefined, `${bind.cmd} 1`);
+          } else {
+            cmd_parse.handle(undefined, bind.cmd);
+          }
+        }
       }
-    }
-    if (bind.mode === 'hold' && keyUpEdge(bind.key)) {
-      cmd_parse.handle(undefined, `${bind.cmd} 0`);
-    }
-  }
-
-  for (let ii = 0; ii < pad_binds.length; ++ii) {
-    let bind = pad_binds[ii];
-    if (padButtonDownEdge(bind.pad)) {
-      if (bind.mode === 'hold') {
-        cmd_parse.handle(undefined, `${bind.cmd} 1`);
-      } else {
-        cmd_parse.handle(undefined, bind.cmd);
+      if (set.upEdge(bindlist.code)) {
+        for (let ii = 0; ii < bindlist.list.length; ++ii) {
+          let bind = bindlist.list[ii];
+          if (bind.mode === 'hold') {
+            cmd_parse.handle(undefined, `${bind.cmd} 0`);
+          }
+        }
       }
-    }
-    if (bind.mode === 'hold' && padButtonUpEdge(bind.pad)) {
-      cmd_parse.handle(undefined, `${bind.cmd} 0`);
     }
   }
 }
